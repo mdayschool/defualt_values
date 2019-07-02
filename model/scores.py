bd3db6220ee78204a5e7da334b1b84d4752805d4
Michael Day <day.michael.b@gmail.com>
2019-07-02 11:56:54 -0500

    test 4 green

--- model/scores.py
+++ model/scores.py
@@ -3,7 +3,8 @@ def score_input(test_name, test_score=0,
             invalid_message='Invalid test score, try again!'):
     """Validates and prints/returns parameters"""
     MIN_SCORE = 0
-    if test_score >= MIN_SCORE:
+    MAX_SCORE = 100
+    if test_score >= MIN_SCORE and test_score <= MAX_SCORE:
         print(test_name + ": 0")
         return {test_name:test_score}
     else:
