bf6bcb3a559ed6fe24a1082af9805b9be4d3d6c3
Michael Day <day.michael.b@gmail.com>
2019-07-02 11:46:23 -0500

    test 2 red

--- model/scores.py
+++ model/scores.py
@@ -1,5 +1,7 @@
 
 def score_input(test_name, test_score=0,
-                invalid_message='Invalid test score, try again!'):
-    pass
+            invalid_message='Invalid test score, try again!'):
+    """Validates and prints/returns parameters"""
+    print(test_name + ": 0")
+    return {test_name:0}
 
--- test/test.py
+++ test/test.py
@@ -5,6 +5,8 @@ from model import scores
 class ScoreTestCase(unittest.TestCase):
     def test_score_input_test_name(self):
         self.assertEqual({'test':0}, scores.score_input('test'))
+    def test_score_input_test_score_valid(self):
+        self.assertEqual({'test':50}, scores.score_input('test', 50))
 
 if __name__ == '__main__':
     unittest.main()
