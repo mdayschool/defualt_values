d3cdd9ece59a26b79ebd58766745d124a431bf0f
Michael Day <day.michael.b@gmail.com>
2019-07-02 11:36:19 -0500

    test 1 red

--- test/test.py
+++ test/test.py
@@ -4,7 +4,7 @@ from model import scores
 
 class ScoreTestCase(unittest.TestCase):
     def test_score_input_test_name(self):
-        self.assertEqual(0, scores.score_input('test'))
+        self.assertEqual({'test':0}, scores.score_input('test'))
 
 if __name__ == '__main__':
     unittest.main()
