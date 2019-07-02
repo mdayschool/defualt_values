aafe2ed393bc9e6eb9a050085f998c6e3b13469a
Michael Day <day.michael.b@gmail.com>
2019-07-02 11:55:53 -0500

    test 4 red

--- test/test.py
+++ test/test.py
@@ -10,6 +10,9 @@ class ScoreTestCase(unittest.TestCase):
     def test_score_input_test_score_below_range(self):
         self.assertEqual('Invalid test score, try again!',
                          scores.score_input('test', -1))
+    def test_score_input_test_score_above_range(self):
+        self.assertEqual('Invalid test score, try again!',
+                         scores.score_input('test', 101))
 
 if __name__ == '__main__':
     unittest.main()
